e6c6c0b133f76eef1aac697d89e1140003057610
James Barnard <james.r.barnard@gmail.com>
2016-04-06 22:28:14 +0100

    Setting up basic start for client unit testing

---
+++ tests/EventsForceClientTest.php
@@ -0,0 +1,25 @@
+<?php
+class EventsForceClientTest extends PHPUnit_Framework_TestCase
+{
+
+    protected $client;
+
+    public function setUp()
+    {
+        $this->client = new EventsForce\Client('client_slug', 'apikey');
+    }
+
+    public function testClientObject()
+    {
+        $this->assertInstanceOf('EventsForce\Client', $this->client);
+    }
+
+    public function testResourcesExtendBase() {
+        $this->assertInstanceOf('EventsForce\Resources\Base', $this->client->events);
+    }
+
+    public function testResourcesInstances()
+    {
+        $this->assertInstanceOf('EventsForce\Resources\Events', $this->client->events);
+    }
+}
